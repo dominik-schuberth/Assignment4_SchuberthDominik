a9aa037bdd19dc012a7e20b4a5985ee0ff862713
schub <me20m018@technikum-wien.at>
2021-05-01 16:27:04 +0200

    Second Commit

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
+# Source: https://blog.quantinsti.com/hierarchical-clustering-python/#agglomerative-hierarchical-clustering
+#Github:
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as sch
 from sklearn.cluster import AgglomerativeClustering
 
-# https://blog.quantinsti.com/hierarchical-clustering-python/#agglomerative-hierarchical-clustering
-# https://towardsdatascience.com/machine-learning-algorithms-part-12-hierarchical-agglomerative-clustering-example-in-python-1e18e0075019
 
+
+
+# Reading values from the input file
 dataset = pd.read_csv('input.csv',delimiter=";", header = None, skiprows=2)
 X = np.array(dataset)
 
-plt.figure('Data')
+# Plot the original data
+plt.figure('Original Data')
+plt.title('Original Data')
 plt.ylabel('y')
 plt.xlabel('x')
 plt.scatter(X[:, 0], X[:, 1], color='blue')
 plt.show()
 
-print("alt", X)
-
+# Fit the model
 cluster = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='ward')
-cluster.fit(X) # the fit methode tells for each data point to which cluster the point belongs to
-print(X)
-labels = cluster.labels_
-print(labels)
+label = cluster.fit_predict(X)
 
-plt.scatter(X[labels == 0, 0], X[labels == 0, 1], marker='o', color='red')
-plt.scatter(X[labels == 1, 0], X[labels == 1, 1], marker='o', color='blue')
-plt.scatter(X[labels == 2, 0], X[labels == 2, 1], marker='o', color='green')
-# plt.scatter(X[labels==3, 0], X[labels==3, 1], s=50, marker='o', color='purple')
-# plt.scatter(X[labels==4, 0], X[labels==4, 1], s=50, marker='o', color='orange')
-
+# Plot the clusters
+plt.figure('Hirachical Clustering')
+plt.scatter(X[label == 0, 0], X[label == 0, 1], s=100, marker='o', color='red', label='Cluster 1')
+plt.scatter(X[label == 1, 0], X[label == 1, 1], s=100, marker='o', color='blue', label='Cluster 2')
+plt.scatter(X[label == 2, 0], X[label == 2, 1], s=100, marker='o', color='green', label='Cluster 3')
+plt.title('Hirachical Clustering')
+plt.legend(loc='upper left')
 plt.ylabel('y')
 plt.xlabel('x')
 plt.show()
 
+# Plot the Dendrogram
+plt.figure('Dendrogram')
 dendrogram = sch.dendrogram(sch.linkage(X, method="ward"))
 plt.title('Dendrogram')
-#plt.xlabel('Customers')
+plt.xlabel('Sample Index')
 plt.ylabel('Euclidean distances')
 plt.show()
